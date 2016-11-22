630d84593b6b81779bf113b8865a159f187dad4c
Nientary <Nientary@yandex.ru>
2016-11-22 21:49:52 +0300

    Проверка форм
    Добавила проверку форм

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ var popup = document.querySelector(".modal-content");
 var overlay = document.querySelector(".overlay");
 var close = popup.querySelector(".modal-content-close");
 var username = popup.querySelector("input[name=name]");
+var email = popup.querySelector("input[name=email]");
+var textarea = popup.querySelector("textarea");
+var form = popup.querySelector("form");
           
 button.addEventListener("click", function (event) {
     event.preventDefault();
@@ -14,11 +17,17 @@ button.addEventListener("click", function (event) {
 close.addEventListener("click", function (event) {
     popup.classList.remove("modal-content-show");
     overlay.classList.remove("overlay-show");
+    username.classList.remove("invalid");
+    email.classList.remove("invalid");
+    textarea.classList.remove("invalid");
 });
 
 overlay.addEventListener("click", function (event) {
     popup.classList.remove("modal-content-show");
     overlay.classList.remove("overlay-show");
+    username.classList.remove("invalid");
+    email.classList.remove("invalid");
+    textarea.classList.remove("invalid");
 });
 
 window.addEventListener("keydown", function (event) {
@@ -26,14 +35,59 @@ window.addEventListener("keydown", function (event) {
         if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
             overlay.classList.remove("overlay-show");
+            username.classList.remove("invalid");
+            email.classList.remove("invalid");
+            textarea.classList.remove("invalid");
         }
     }
 });
 
+form.addEventListener("submit", function(event) {
+    if (!username.value || !email.value || !textarea.value) {
+    event.preventDefault();
+        if (!username.value) {
+            username.classList.add("invalid");
+            }
+        if (!email.value) {
+            email.classList.add("invalid");
+        }
+        if (!textarea.value) {
+            textarea.classList.add("invalid");
+        }
+    }
+});
+
+
+username.addEventListener ("keydown", function(event) {
+    if (username.value) {
+        if (username.classList.contains("invalid")) {
+            username.classList.remove("invalid");
+        }
+    }
+});
+
+email.addEventListener ("keydown", function(event) {
+    if (email.value) {
+        if (email.classList.contains("invalid")) {
+            email.classList.remove("invalid");
+        }
+    }
+});
+
+textarea.addEventListener ("keydown", function(event) {
+    if (textarea.value) {
+        if (textarea.classList.contains("invalid")) {
+            textarea.classList.remove("invalid");
+        }
+    }
+});
+
+
 function initialize() {
   var mapOptions = {
     zoom: 17,
-    center: new google.maps.LatLng(59.9388329, 30.3235549)
+    center: new google.maps.LatLng(59.9388329, 30.3235549),
+    scrollwheel: false
   }
   var map = new google.maps.Map(document.getElementById("map-canvas"),
                                 mapOptions);
